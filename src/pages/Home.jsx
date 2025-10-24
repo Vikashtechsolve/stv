ab3100d1c76435f0f036634082e7ce3b47c9a876
Mohd Mustakim <Mustakim.shaikh.prof@gmail.com>
2025-10-24 17:07:12 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import Navbar from '../component/Navbar'
+import React, { useEffect } from 'react'
+import { useLocation } from "react-router-dom";
+
 import Layout from '../component/Layout'
 import ContactForm from '../component/ContactForm'
 import Menebership from '../component/Menebership'
@@ -15,22 +16,71 @@ import WhatWeOffer from '../component/WhatWeOffer'
 import MasterClass from '../component/MasterClass'
 
 const Home = () => {
-    return (
-        <Layout>
-            <Hero/>
-            <StatsSection/>
-            <TrustedInstitutions/>
-            <WhatWeOffer/>
-            <MasterClass/>
-            <HowWeWork/>
-            <ShapeYourJourney/>
-            <SuccessStories/>
-            <WhyTrustUs/>
-            <Mentors/>
-            <Menebership/>
-            <ContactForm />
-        </Layout>
-    )
-}
 
-export default Home
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.scrollTo) {
+      const sectionId = location.state.scrollTo;
+
+      // delay ensures layout is loaded before scrolling
+      setTimeout(() => {
+        const el = document.getElementById(sectionId);
+        if (el) {
+          el.scrollIntoView({ behavior: "smooth" });
+        }
+      }, 200);
+
+      // Clear state to avoid repeated scrolling when navigating back
+      window.history.replaceState({}, document.title);
+    }
+  }, [location]);
+
+  return (
+    <Layout>
+
+      <div id="home">
+        <Hero />
+      </div>
+
+      <StatsSection />
+
+      <div id="institutions">
+        <TrustedInstitutions />
+      </div>
+
+      <div id="offer">
+        <WhatWeOffer />
+      </div>
+
+      <MasterClass />
+
+      <div id="howWeWork">
+        <HowWeWork />
+      </div>
+
+      <ShapeYourJourney />
+
+      <div id="successStories">
+        <SuccessStories />
+      </div>
+
+      <div id="whyTrustUs">
+        <WhyTrustUs />
+      </div>
+
+      <div id="mentors">
+        <Mentors />
+      </div>
+
+      <Menebership />
+
+      <div id="contact">
+        <ContactForm />
+      </div>
+
+    </Layout>
+  );
+};
+
+export default Home;
