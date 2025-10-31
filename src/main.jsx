3ab5e8b68625d3dda621cc0e0510aefdb349aa2e
Amitvaghamshi <amitvaghamshi9@gmail.com>
2025-10-31 18:09:19 +0530

    hm

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <HelmetProvider> */}
+    <HelmetProvider>
       <App />
-    {/* </HelmetProvider> */}
+    </HelmetProvider>
   </StrictMode>
 )
 
